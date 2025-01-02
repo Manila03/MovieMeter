41f6883c8b8d0a8039e5be3446ffa73666340b6e
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2025-01-02 00:02:46 -0300

    implementacion de paginacion

--- src/main/java/com/uade/tpo/demo/service/critic/CriticService.java
+++ src/main/java/com/uade/tpo/demo/service/critic/CriticService.java
@@ -3,6 +3,9 @@ package com.uade.tpo.demo.service.critic;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 import com.uade.tpo.demo.entity.Critic;
 import com.uade.tpo.demo.entity.dto.CriticRequest;
 
